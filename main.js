5fbe2458a3444322df3e8eaad244132973f7f624
Mehfuz Hossain <mehfuz@gmail.com>
2017-02-12 21:58:11 -0800

    apiai

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ var Bot = (function () {
     function Bot(args) {
         this.args = args;
     }
+    Object.defineProperty(Bot.prototype, "extract", {
+        get: function () {
+            return "message.text";
+        },
+        enumerable: true,
+        configurable: true
+    });
     Bot.prototype.execute = function (cb) {
-        throw "Nothing Implemented";
+        var args = this.args;
+        if (args.apiai) {
+            var result = args.apiai.result;
+            for (var _i = 0, _a = result.fulfillment.messages; _i < _a.length; _i++) {
+                var message = _a[_i];
+                if (message.payload.facebook) {
+                    return cb(message.payload.facebook);
+                }
+            }
+            return cb({
+                text: result.fulfillment.speech
+            });
+        }
     };
     return Bot;
 }());
--- main.ts
+++ main.ts
@@ -6,11 +6,29 @@ export class Bot {
 
   private args: any;
 
+  get extract(){
+    return "message.text";
+  }
+
   constructor(args:Object){
     this.args = args;
   }
 
   execute(cb:any){
-    throw "Nothing Implemented";
+    let args = this.args;
+
+    if (args.apiai){
+      let result  = args.apiai.result;
+
+        for (let message of result.fulfillment.messages){
+          if (message.payload && message.payload.facebook){
+            return cb(message.payload.facebook);
+          }
+        }
+
+        return cb({
+            text : result.fulfillment.speech
+        });
+    }
   }
 }
